550ee9c76b521573e0a2efb24a0ec8731767a0bc
Maciej Krawczyk <kontakt_maciek@tlen.pl>
2023-04-01 20:57:34 +0200

    first commit

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.DTOs.Character;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        Task<ServiceResponse<List<GetCharacterDTO>>> GetAllCharacters();
+        Task<ServiceResponse<GetCharacterDTO>> GetCharacterById(int id);
+        Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter);
+    }
+}
